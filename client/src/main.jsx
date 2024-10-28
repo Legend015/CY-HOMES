e5239319a900f1aece6443ed8e4f82da6f23b210
Legend015 <yashlanjewar1524@gmail.com>
2024-10-28 21:49:16 +0530

    Homecard created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Navbr from "./component/Navbr.jsx";
 import Searchbr from "./component/Searchbr.jsx";
-
+import HomeCard from "./component/Homecard.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbr></Navbr>
     <Searchbr></Searchbr>
+    <div className="d-flex justify-content-center gap-4 my-4">
+      <HomeCard></HomeCard>
+      <HomeCard></HomeCard>
+    </div>
   </StrictMode>
 );
